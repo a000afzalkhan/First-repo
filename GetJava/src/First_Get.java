e39b5ae8f578f810deca8ede36c9216d8791fe11
000afzalkhan <000heyafzal@gmail.com>
2025-09-15 02:29:05 +0530

    Add var data type

--- GetJava/src/First_Get.java
+++ GetJava/src/First_Get.java
@@ -4,7 +4,11 @@ public class First_Get {
         int a = 1;
         int b = 2;
 
+        var x ="Afzal";
+
         int c = a+b;
+
+        System.out.println(x);
 
         System.out.println("Sum ( a + b ) = " + c);
         System.out.println("Total sum = " + c);
